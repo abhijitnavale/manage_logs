575eb2a05f88172fab96ac4d3312b23d6e49975d
Abhijit Sharad Navale <abhijit.foss@gmail.com>
2017-04-05 16:56:13 +0530

    Delete files having size greaten than configured

--- manage_logs.sh
+++ manage_logs.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/sh
 
+#LOG_FILE="mylogs.log"
+#exec 3>&1 1>>${LOG_FILE} 2>&1
+
 if [ ! -e managelogs.config ]; then
     echo "Configuration file not found"
     exit 1
@@ -14,15 +17,35 @@ line=$(head -n 1 managelogs.config)
 
 read -a arr <<<$line
 
+function send_deletion_email {
+    
+}
+function manage_files_per_size {
+    for filename in "$2"*; do
+        filesize=$(($( stat -c '%s' $filename) / 1024 / 1024 ))
+        if [ $filesize -gt $3 ]; then
+            echo $filename $filesize "size is greater than configured " $3
+            echo "Deleting " $filename
+            rm -f $filename
+            send_deletion_email $filename $filesize $3
+        else
+            echo $filename "size is less than " $filesize
+        fi
+        
+    done
+}
+
 function process_app_log {
-    echo "Process app log for" $1
+    echo "Processing app log for" $1
     case $1 in
         "apache")
-            echo $apache_days
+            manage_files_per_size $1 "/var/log/abhijit/apache/" $apache_size
+            # manage_files_per_days
             ;;
-        "nginx")
-            echo $nginx_size
-            ;;
+        # "nginx")
+        #     manage_files_per_size $1 "/var/log/abhijit/nginx/" $nginx_size
+        #     # manage_files_per_days
+        #     ;;
         esac
 }
 
